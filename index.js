20e84488af2607cb4d9e93819d45827bf8556998
Thane <thabay0606@students.maquoketaschools.org>
2025-11-12 14:12:10 -0600

    Added and/or statements to taxable and isVehicle functions.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function taxable(state, taxExempt) {
+    if (state == IA && taxExempt == false){
+        return "is taxable";
+    }
+    if (state == FL && taxExempt == true){
+        return "is taxable";
+    }
+}
 
+function isVehicle(hasWheels, canFly, canSwim){
+    if (hasWheels == true || canFly == true || canSwim == true){
+        return "is vehicle";
+    }
+}
